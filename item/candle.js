7265b07ea630ef97cd31dcaea13b4e04c4c3f06d
muhamed <muhamed.haseljic.20@size.ba>
2024-12-09 20:04:09 +0100

    Day 9: Glove

--- item/candle.js
+++ item/candle.js
@@ -3,7 +3,7 @@ function drawCandle(ctx, x, y, size, hue) {
     const left = x -size / 2;
     const bottom = y +size / 2;
     const right = x+size/2;
-    ctx.strokeRect(top, left, size, size);
+    //ctx.strokeRect(top, left, size, size);
 
     const stick = {
         width: size * 0.3,
